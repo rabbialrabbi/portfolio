539b6ca866f8ab57df741cce89917f1baabcd36e
Rabbial Anower Home <rabbialrabbi@gmail.com>
2020-06-08 14:24:02 +0600

    starter

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Profile</title>
+</head>
+<body>
+<div id="app">
+<section>
+    <div class="intro">
+
+        <nav class="intro-nav">
+            <div class="row">
+                div.col-
+            </div>
+            <ul class="intro-nav_left">
+                <li class="intro-nav_but"><a href="">RAP</a></li>
+            </ul>
+            <ul class="intro-nav_right">
+                <li class="intro-nav_but"><a href="">RAP</a></li>
+                <li class="intro-nav_but"><a href="">Project</a></li>
+                <li class="intro-nav_but"><a href="">About</a></li>
+                <li class="intro-nav_but"><a href="">Blog</a></li>
+                <li class="intro-nav_but"><a href="">Contact</a></li>
+            </ul>
+        </nav>
+
+    </div>
+    <svg
+        preserveAspectRatio="none"
+        viewBox="0 0 100 102"
+        height="50"
+        width="100%"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        class="intro-svg"
+    >
+        <path d="M0 0 L50 100 L100 0 Z" ></path>
+    </svg>
+
+
+</section>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
