477854d3949b20fd76f8ad2e561c619ad375f20f
Rabbial Anower Home <rabbialrabbi@gmail.com>
2020-06-09 14:02:51 +0600

    Working at Services

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,40 +11,73 @@
 </head>
 <body>
 <div id="app">
-<section>
-    <div class="intro">
+    <section>
+        <div class="intro">
+            <nav class="intro-nav">
+                <div class="row">
+                    <div class="col-4">
+                        <ul class="intro-nav_logo">
+                            <li class="intro-nav_left-but"><a href="">R. Anowar</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-6 intro-nav_right">
+                        <ul>
+                            <li class="intro-nav_right-but"><a href="">Project</a></li>
+                            <li class="intro-nav_right-but"><a href="">About</a></li>
+                            <li class="intro-nav_right-but"><a href="">Blog</a></li>
+                            <li class="intro-nav_right-but"><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-        <nav class="intro-nav">
-            <div class="row">
-                div.col-
+            <div class="intro-text">
+                <p>Hello, I'm <span>Rabbial Anowar</span></p>
+                <p>A Full-Stack Web Developer</p>
+                <button>View Profile</button>
             </div>
-            <ul class="intro-nav_left">
-                <li class="intro-nav_but"><a href="">RAP</a></li>
-            </ul>
-            <ul class="intro-nav_right">
-                <li class="intro-nav_but"><a href="">RAP</a></li>
-                <li class="intro-nav_but"><a href="">Project</a></li>
-                <li class="intro-nav_but"><a href="">About</a></li>
-                <li class="intro-nav_but"><a href="">Blog</a></li>
-                <li class="intro-nav_but"><a href="">Contact</a></li>
-            </ul>
-        </nav>
+        </div>
+        <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 100 102"
+            height="50"
+            width="100%"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            class="intro-svg"
+        >
+            <path d="M0 0 L50 100 L100 0 Z" ></path>
+        </svg>
+    </section>
+    <section>
+        <div class="container">
+            <div class="header">
+                <p>SERVICES</p>
+                <div class="u-line"></div>
+            </div>
+            <div class="row service">
+                <div class="col-4 service-item">
+                    <i>????</i>
+                    <h4>FRONT END</h4>
+                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                </div>
+                <div class="col-4 service-item">
+                    <i>????</i>
+                    <h4>FRONT END</h4>
+                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                </div>
+                <div class="col-4 service-item">
+                    <i>????</i>
+                    <h4>FRONT END</h4>
+                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                </div>
+            </div>
+        </div>
 
+    </section>
+    <div class="space">
     </div>
-    <svg
-        preserveAspectRatio="none"
-        viewBox="0 0 100 102"
-        height="50"
-        width="100%"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        class="intro-svg"
-    >
-        <path d="M0 0 L50 100 L100 0 Z" ></path>
-    </svg>
-
-
-</section>
 </div>
 <script src="{{asset('js/app.js')}}"></script>
 </body>
