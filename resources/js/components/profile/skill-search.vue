5913d1272fd2787dae8309bbb9e40d4286b4079b
Rabbial Anower Home <rabbialrabbi@gmail.com>
2020-06-18 06:32:29 +0600

    Working with Search

---
+++ resources/js/components/profile/skill-search.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-sm-6 skill_tag">
+        <h4>Looking for particular Skill ?</h4>
+        <input type="text" placeholder="Write particular skill you want" v-model="searchKey">
+        <div class="skill_tag-list">
+            <i class="fas fa-tags" v-for="skill in skillFilter" > {{skill}}</i>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "skill-search",
+        data(){
+            return {
+                searchKey:'',
+                skills: [
+                    'HTML','CSS','JavaScript','Vue', 'Ajax', 'JSON', 'PHP', 'Unit', 'Laravel',
+                    'JQuery', 'OOP', 'Regex', 'Glup', 'Git', 'SQL', 'MySql', 'PDO', 'Illustrator',
+                    'Photoshop',
+                ]
+            }
+        },
+        methods:{
+
+        },
+        computed:{
+            skillFilter(){
+                return this.skills.filter(skill=>{
+                    skill.toLowerCase()
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
     <title>Profile</title>
 </head>
 <body>
@@ -50,34 +51,149 @@
             <path d="M0 0 L50 100 L100 0 Z" ></path>
         </svg>
     </section>
-    <section>
+
+    <section class="service">
         <div class="container">
             <div class="header">
                 <p>SERVICES</p>
                 <div class="u-line"></div>
             </div>
-            <div class="row service">
-                <div class="col-4 service-item">
-                    <i>????</i>
-                    <h4>FRONT END</h4>
-                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                <div class="glide">
+                    <div class="glide__track">
+                        <ul class="row glide__slides">
+                            <li class="col-sm-4 service-item glide__slide">
+                                <img src="{{asset('image/fe_logo.png')}}" alt="FrontEnd Logo">
+                                <h5>FRONT END</h5>
+                                <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                            </li>
+                            <li class="col-sm-4 service-item glide__slide">
+                                <img src="{{asset('image/be_logo.png')}}" alt="BackEnd Logo">
+                                <h5>BACK END</h5>
+                                <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                            </li>
+                            <li class="col-sm-4 service-item glide__slide">
+                                <img src="{{asset('image/db_logo.png')}}" alt="Database Logo">
+                                <h5>DATABASE DESIGH</h5>
+                                <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-4 service-item">
-                    <i>????</i>
-                    <h4>FRONT END</h4>
-                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
-                </div>
-                <div class="col-4 service-item">
-                    <i>????</i>
-                    <h4>FRONT END</h4>
-                    <p>Clean, modern designs - optimized for performance, search engines, and converting users to customers.</p>
-                </div>
+        </div>
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             viewBox="0 0 1920 80" style="enable-background:new 0 0 1920 80;" xml:space="preserve">
+                <polygon points="0,0 960,39.6 1920,80 0,80 "/>
+            </svg>
+    </section>
+    <section class="skill">
+        <div class="container">
+            <div class="header pb-5">
+                <p>ABOUT</p>
+                <div class="u-line"></div>
             </div>
+
+            <div class="row">
+                <div class="col-sm-6 skill_parameter">
+                        <h4>Skills</h4>
+                        <p>HTML (60%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill nok"></div>
+                        </div>
+                        <p>CSS (30%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill htc"></div>
+                        </div>
+                        <p>JavaScript (80%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill sam" ></div>
+                        </div>
+                        <p>Vue.js (80%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill sam" ></div>
+                        </div>
+                        <p>JQuery (80%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill sam" ></div>
+                        </div>
+                        <p>PHP (70%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill iph"></div>
+                        </div>
+                        <p>Laravel (10%)</p>
+                        <div class="skill_parameter-body">
+                            <div class="skill_parameter-fill lgp"></div>
+                        </div>
+                    </div>
+
+                    <skill-search></skill-search>
+
+
+                </div>
+        </div>
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             viewBox="0 0 1920 80" style="enable-background:new 0 0 1920 80;" xml:space="preserve">
+                <polygon points="0,0 960,39.6 1920,80 0,80 "/>
+            </svg>
+    </section>
+
+    <section class="portfolio">
+        <div class="container">
+            <div class="header_white pb-5">
+                <p>PORTFOLIO</p>
+                <div class="u-line"></div>
+            </div>
+
+            <div class="row justify-content-md-center no-gutters portfolio_body ">
+                <img class="col-sm-4" src="{{asset('image/ticket_front.jpg')}}" alt="">
+                <img class="col-sm-4" src="{{asset('image/admin_front.jpg')}}" alt="">
+                <img class="col-sm-4" src="{{asset('image/monster_log.jpg')}}" alt="">
+                <img class="col-sm-4" src="{{asset('image/lov_front.jpg')}}" alt="">
+                <img class="col-sm-4" src="{{asset('image/legend_front.jpg')}}" alt="">
+            </div>
+        </div>
+        <svg
+            preserveAspectRatio="none"
+            viewBox="0 0 100 102"
+            height="50"
+            width="100%"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            class="intro-svg"
+        >
+            <path d="M0 0 L50 100 L100 0 Z" ></path>
+        </svg>
+    </section>
+
+    <section class="contact">
+        <div class="container">
+            <div class="header pb-5 setting">
+                <p>Get In Touch</p>
+                <div class="u-line"></div>
+            </div>
+
+            <form action="">
+                <div class="contact_form-body ">
+                    <div class="row justify-content-center pt-5">
+                        <h6>Have A Project You Like To Discuss ?</h6>
+                    </div>
+
+                    <div class="row justify-content-end">
+                        <div><input type="text" placeholder="Name"></div>
+                        <div><input type="text" placeholder="Email"></div>
+                        <div><textarea name="" id="" rows="6" placeholder="Message"></textarea></div>
+                        <div><input type="submit" name="send" value="Send"></div>
+                    </div>
+                </div>
+            </form>
         </div>
 
     </section>
-    <div class="space">
-    </div>
+
+    <section class="contact_button">
+        <div class="container">
+            <p>All Right reserved by Rabbial Anower @ 2020</p>
+        </div>
+    </section>
 </div>
 <script src="{{asset('js/app.js')}}"></script>
 </body>
